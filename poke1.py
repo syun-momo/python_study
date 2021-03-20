7fa764fdfb6999297a3334359144e48095110c66
syun-momo <72676307+syun-momo@users.noreply.github.com>
2021-03-20 19:44:32 +0900

    移動範囲を制御

--- poke1.py
+++ poke1.py
@@ -4,6 +4,9 @@ from game import *
 # 画像の読み込みpoke
 image_player = image('ddpoke.png')
 
+#　画面のサイズ
+SW, SH = 1, 9/16
+
 # 自機の処理
 def player(p):
     # 自機の移動速度
@@ -17,6 +20,9 @@ def player(p):
         p.y += v
     if key(DOWN):
         p.y -= v
+    # 移動可能範囲指定
+    p.x = max(-SW+p.sx, min(SW-p.sx, p.x))
+    p.y = max(-SH+p.sy, min(SH-p.sy, p.y))
 
 # ゲームの開始処理
 def start():
