6484c775aa37779be5e272a18ae1300ba9a8de97
syun-momo <72676307+syun-momo@users.noreply.github.com>
2021-03-20 18:27:33 +0900

    コードコメント記載

--- poke1.py
+++ poke1.py
@@ -1,10 +1,14 @@
+# 機能の読み込み
 from game import *
 
+# 画像の読み込みpoke
 image_player = image('ddpoke.png')
 
+# 自機の処理
 def player(p):
     p.x += p.vx
 
+# ゲームの開始処理
 def start():
     add(player, image_player, 0.1, -1.1, 0, 0.01, 0)
 
